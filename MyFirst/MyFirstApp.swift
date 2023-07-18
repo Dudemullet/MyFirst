05175f89ef3c3e580093160717fffddf82ea6716
Pipe Gutierrez <elpaip@gmail.com>
2023-07-18 07:26:42 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Pipe Gutierrez on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
